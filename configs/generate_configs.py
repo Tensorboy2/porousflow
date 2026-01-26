512b63084a85ed02fe3b8d640dd579fb7b6aa6ca
Tensorboy2 <svargdal@online.no>
2026-01-26 13:24:18 +0100

    smaller bs

--- configs/generate_configs.py
+++ configs/generate_configs.py
@@ -224,7 +224,7 @@ TASK_CONFIGS = {
     "dispersion": {
         "learning_rate": 5e-3,
         "weight_decay": 0.2,
-        "batch_size": 2048,
+        "batch_size": 512,
         "num_epochs": 500,
         "decay": "cosine",
         "warmup_steps": 1_000,
