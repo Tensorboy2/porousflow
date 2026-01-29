c3f5d03a16153db704499430dd5334aa03c04586
Tensorboy2 <svargdal@online.no>
2026-01-29 16:41:01 +0100

    now

--- src/ml/trainer.py
+++ src/ml/trainer.py
@@ -225,14 +225,14 @@ class Trainer:
             # scaled_outputs = torch.sign(outputs_fp32) * torch.sqrt(torch.abs(outputs_fp32) + 1e-8)
             # scaled_D = torch.sign(D_fp32) * torch.sqrt(torch.abs(D_fp32) + 1e-8)
 
-            scaled_outputs = self.scale(outputs_fp32)
+            # scaled_outputs = self.scale(outputs_fp32)
             # scaled_outputs = self.scale(scaled_outputs)
             scaled_D = self.scale(D_fp32)
             # scaled_D = self.scale(scaled_D)
             # scaled_outputs = torch.sign(outputs_fp32) * torch.log1p(torch.abs(outputs_fp32)/100)
             # scaled_D = torch.sign(D_fp32) * torch.log1p(torch.abs(D_fp32)/100)
 
-            loss = self.criterion(scaled_outputs, scaled_D)
+            loss = self.criterion(outputs_fp32, scaled_D)
             # loss = self.criterion(outputs_fp32, D_fp32)
             running_loss += loss.item() * B
             total_samples += B
