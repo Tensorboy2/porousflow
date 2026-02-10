475acf0876821f9c1423919eae00291038749dd0
Tensorboy2 <svargdal@online.no>
2026-02-10 14:38:32 +0100

    added the translation to permeability

--- src/ml/dataaugmentations.py
+++ src/ml/dataaugmentations.py
@@ -8,7 +8,8 @@ import torchvision.transforms.functional as tf
 import random
 
 class PermeabilityTransform:
-    def __init__(self):
+    def __init__(self, enable_shift=True):
+        self.enable_shift = enable_shift
         self.permutations = [
             ('I', lambda img: img, lambda K: K),
             ('R', lambda img: tf.rotate(img, 90),
@@ -26,10 +27,16 @@ class PermeabilityTransform:
             ('HRRR', lambda img: tf.rotate(tf.hflip(img), 270),
                      lambda K: torch.tensor([K[3], K[2], K[1], K[0]])),  # H + 270°
         ]
+    def periodic_shift(self, img):
+        sx = random.randint(0, img.shape[-2] - 1)
+        sy = random.randint(0, img.shape[-1] - 1)
+        return torch.roll(img, shifts=(sx, sy), dims=(-2, -1))
 
     def __call__(self, image, K):
         # Randomly choose one of the 8 permutations
         _, img_transform, k_transform = random.choice(self.permutations)
+        if self.enable_shift:
+            image = self.periodic_shift(image)
         image = img_transform(image)
         K = k_transform(K)
         return image, K
